d5300ab0ab4fed11a278e45a23281496fd7a8dd6
Radu Achim <radu.achim@iteam.se>
2014-11-14 12:06:32 +0100

    Finalize response
    Override either res.send or res.end to wait for the store update
    Fixed tests

--- test/mongostore.js
+++ test/mongostore.js
@@ -38,11 +38,11 @@ describe('MongoStore', function () {
 
     mongoData = {
       foo: {
-        expires: Date.now() + 21 * 60 * 10000,
+        expires: new Date(Date.now() + 21 * 60 * 10000),
         data: { herp: 'derp', token: 'foo' }
       },
       bar: {
-        expires: Date.now() - 10,
+        expires: new Date(Date.now() - 10),
         data: { baz: true, token: 'bar' }
       }
     };
@@ -60,8 +60,10 @@ describe('MongoStore', function () {
     });
     it('returns an object corresponding to the passed key', function () {
       var listener = sinon.spy();
-      store.get('foo').then(listener);
+      var fail = sinon.spy();
+      store.get('foo').then(listener).catch(fail);
       sessionCollection.findOne.resolve(mongoData.foo);
+      expect(listener).calledOnce;
       expect(listener).calledWith(mongoData.foo);
     });
     it('returns an empty object if session has expired', function () {
@@ -85,6 +87,11 @@ describe('MongoStore', function () {
       sessionCollection.findOne.resolve();
       expect(sessionCollection.save).not.called;
     });
+    it('does nothing if no change and session is an empty object', function () {
+      store.update('baz', {});
+      sessionCollection.findOne.resolve();
+      expect(sessionCollection.save).not.called;
+    });
     it('updates data and writes to database if changed', function () {
       mongoData.foo.data.herp = 'doge';
 
@@ -96,10 +103,10 @@ describe('MongoStore', function () {
     });
     it('updates expires and writes to database if time to expiry < activeDuration', function () {
       sandbox.stub(Date, 'now').returns(1000);
-      mongoData.foo.expires = 1100;
+      mongoData.foo.expires = new Date(1100);
       store.update('foo');
       sessionCollection.findOne.resolve(mongoData.foo);
-      expect(sessionCollection.save).calledOnce;
+      expect(sessionCollection.save, 'sessionCollection.save').calledOnce;
       mongoData.foo.expires = 5 * 60 * 1000 + 1100;
       expect(sessionCollection.save).calledWith(mongoData.foo);
     });
